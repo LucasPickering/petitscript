4d9403bf6819ba008a4bd6ecae4ddd8a43c5a073
Lucas Pickering <lucas@lucaspickering.me>
2025-01-30 18:32:38 -0500

    Add shell parser

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@
 
 mod ast;
 mod error;
+mod parse;
 mod runtime;
 mod stdlib;
 mod value;
@@ -12,3 +13,33 @@ pub use crate::{
     runtime::{module::Module, Runtime},
     value::{Array, Function, JsString, Number, Object, Value},
 };
+use std::{borrow::Cow, ffi::OsStr, fs, path::Path};
+
+/// A source of source code. E.g. a string literal or a file path
+pub trait Source {
+    /// TODO
+    fn name(&self) -> Option<&str>;
+
+    /// TODO
+    fn text(&self) -> Result<Cow<'_, str>>;
+}
+
+impl Source for String {
+    fn name(&self) -> Option<&str> {
+        None
+    }
+
+    fn text(&self) -> Result<Cow<'_, str>> {
+        Ok(self.as_str().into())
+    }
+}
+
+impl Source for &Path {
+    fn name(&self) -> Option<&str> {
+        self.file_name().and_then(OsStr::to_str)
+    }
+
+    fn text(&self) -> Result<Cow<'_, str>> {
+        Ok(fs::read_to_string(self)?.into())
+    }
+}
---
+++ src/parse.rs
@@ -0,0 +1,8 @@
+use crate::{ast::Script, Result, Source};
+
+/// Parse source code into an Abstract Syntax Tree
+pub fn parse(source: impl Source) -> Result<Script> {
+    let name = source.name();
+    let code = source.text()?;
+    todo!()
+}
